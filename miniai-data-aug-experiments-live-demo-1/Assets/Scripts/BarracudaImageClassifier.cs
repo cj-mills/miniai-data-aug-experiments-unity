2cf2c77e88e98f005df36401595daa20a24e5828
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-03-22 16:08:49 -0700

    Update BarracudaImageClassifier.cs

--- miniai-data-aug-experiments-live-demo-1/Assets/Scripts/BarracudaImageClassifier.cs
+++ miniai-data-aug-experiments-live-demo-1/Assets/Scripts/BarracudaImageClassifier.cs
@@ -45,15 +45,6 @@ public class BarracudaImageClassifier : MonoBehaviour
     [Tooltip("Option to print debugging messages to the console")]
     public bool printDebugMessages = true;
 
-    [Header("Webcam")]
-    [Tooltip("Option to use webcam as input")]
-    public bool useWebcam = false;
-    [Tooltip("Requested webcam dimensions")]
-    public Vector2Int webcamDims = new Vector2Int(1280, 720);
-    [Tooltip("Requested webcam framerate")]
-    [Range(0, 60)]
-    public int webcamFPS = 60;
-
     [Header("GUI")]
     [Tooltip("Option to display predicted class")]
     public bool displayPredictedClass = true;
@@ -67,17 +58,6 @@ public class BarracudaImageClassifier : MonoBehaviour
     [Tooltip("Time in seconds between refreshing fps value")]
     [Range(0.01f, 1.0f)]
     public float fpsRefreshRate = 0.1f;
-    [Tooltip("Toggle to use webcam as input source")]
-    public Toggle useWebcamToggle;
-    [Tooltip("Dropdown menu with available webcam devices")]
-    public Dropdown webcamDropdown;
-
-    // Webcam device list
-    private WebCamDevice[] webcamDevices;
-    // Live webcam video input
-    private WebCamTexture webcamTexture;
-    // Name of the current webcam device
-    private string currentWebcam;
 
     // Dimensions of the test image
     private Vector2Int imageDims;
@@ -125,95 +105,6 @@ public class BarracudaImageClassifier : MonoBehaviour
 
 
     /// <summary>
-    /// Initialize the selected webcam device
-    /// </summary>
-    /// <param name="deviceName">The name of the selected webcam device</param>
-    private void InitializeWebcam(string deviceName)
-    {
-        // Stop playing any existing webcam texture
-        if (webcamTexture && webcamTexture.isPlaying) webcamTexture.Stop();
-
-        // Create new webcam texture with specified device name, dimensions, and FPS
-        webcamTexture = new WebCamTexture(deviceName, webcamDims.x, webcamDims.y, webcamFPS);
-
-        // Start playing the webcam texture
-        webcamTexture.Play();
-
-        // Update the useWebcam flag based on whether the webcam is playing
-        useWebcam = webcamTexture.isPlaying;
-        useWebcamToggle.SetIsOnWithoutNotify(useWebcam);
-
-        // Log whether the webcam is playing or not
-        Debug.Log(useWebcam ? "Webcam is playing" : "Webcam not playing, option disabled");
-    }
-
-
-    /// <summary>
-    /// Resize and position an in-scene screen object
-    /// </summary>
-    private void InitializeScreen()
-    {
-        // Set the texture of the screen to either the webcam texture or the image texture
-        screen.gameObject.GetComponent<MeshRenderer>().material.mainTexture = useWebcam ? webcamTexture : imageTexture;
-
-        // Set the dimensions of the screen to either the dimensions of the webcam texture or the image texture
-        screenDims = useWebcam ? new Vector2Int(webcamTexture.width, webcamTexture.height) : imageDims;
-
-        // Set the y-rotation of the screen to 180 degrees if using the webcam, or 0 degrees if using the image texture
-        float yRotation = useWebcam ? 180f : 0f;
-
-        // Set the z-scale of the screen to -1 if using the webcam, or 1 if using the image texture
-        float zScale = useWebcam ? -1f : 1f;
-
-        // Apply the rotation and scale to the screen
-        screen.rotation = Quaternion.Euler(0, yRotation, 0);
-        screen.localScale = new Vector3(screenDims.x, screenDims.y, zScale);
-
-        // Position the screen in the center of the screen with a z-value of 1
-        screen.position = new Vector3(screenDims.x / 2, screenDims.y / 2, 1);
-    }
-
-
-    /// <summary>
-    /// Initialize the GUI dropdown list
-    /// </summary>
-    private void InitializeDropdown()
-    {
-        // Create a list to store the names of all available webcams
-        List<string> webcamNames = new List<string>();
-
-        // Populate the list with the names of all available webcams
-        foreach (WebCamDevice device in webcamDevices) webcamNames.Add(device.name);
-
-        // Clear any existing options from the webcam dropdown
-        webcamDropdown.ClearOptions();
-
-        // Add the list of webcam names as options to the webcam dropdown
-        webcamDropdown.AddOptions(webcamNames);
-
-        // Set the selected value of the webcam dropdown to the index of the current webcam in the list of webcam names
-        webcamDropdown.SetValueWithoutNotify(webcamNames.IndexOf(currentWebcam));
-    }
-
-
-    /// <summary>
-    /// Resize and position the main camera based on an in-scene screen object
-    /// </summary>
-    /// <param name="screenDims">The dimensions of an in-scene screen object</param>
-    private void InitializeCamera(Vector2Int screenDims, string cameraName = "Main Camera")
-    {
-        // Get a reference to the Main Camera GameObject
-        GameObject camera = GameObject.Find(cameraName);
-        // Adjust the camera position to account for updates to the screenDims
-        camera.transform.position = new Vector3(screenDims.x / 2, screenDims.y / 2, -10f);
-        // Render objects with no perspective (i.e. 2D)
-        camera.GetComponent<Camera>().orthographic = true;
-        // Adjust the camera size to account for updates to the screenDims
-        camera.GetComponent<Camera>().orthographicSize = screenDims.y / 2;
-    }
-
-
-    /// <summary>
     /// Initialize an interface to execute the specified model using the specified backend
     /// </summary>
     /// <param name="model">The target model representation</param>
@@ -254,22 +145,6 @@ public class BarracudaImageClassifier : MonoBehaviour
             processingShader.SetBuffer(kernelIndex, "std", std_buffer);
         }
 
-        // Get the source image texture and dimensions
-        imageTexture = screen.gameObject.GetComponent<MeshRenderer>().material.mainTexture;
-        imageDims = new Vector2Int(imageTexture.width, imageTexture.height);
-
-        // Get the list of available webcam devices
-        webcamDevices = WebCamTexture.devices;
-        currentWebcam = webcamDevices[0].name;
-        useWebcam = webcamDevices.Length > 0 ? useWebcam : false;
-
-        // Initialize the webcam if available
-        if (useWebcam) InitializeWebcam(currentWebcam);
-
-        // Initialize the screen and camera
-        InitializeScreen();
-        InitializeCamera(screenDims);
-
         // Load and modify the model, and initialize the engine for executing the model
         Model m_RunTimeModel = ModelLoader.Load(modelAsset);
         string outputLayer = m_RunTimeModel.outputs[outputLayerIndex];
@@ -284,9 +159,6 @@ public class BarracudaImageClassifier : MonoBehaviour
         // Initialize the GPU and CPU output textures
         outputTextureGPU = RenderTexture.GetTemporary(1, classes.Length, 24, RenderTextureFormat.ARGBHalf);
         outputTextureCPU = new Texture2D(1, classes.Length, TextureFormat.RGBAHalf, false);
-
-        // Initialize the webcam dropdown list
-        InitializeDropdown();
     }
 
 
@@ -422,38 +294,11 @@ public class BarracudaImageClassifier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check if there are any available webcam devices and set `useWebcam` accordingly
-        useWebcam = webcamDevices.Length > 0 ? useWebcam : false;
-        if (useWebcam)
-        {
-            // Initialize webcam if it is not already playing
-            if (!webcamTexture || !webcamTexture.isPlaying) InitializeWebcam(currentWebcam);
-
-            // Return if webcam is not initialized
-            if (webcamTexture.width <= 16) return;
-
-            // Resize and position screen and camera if dimensions don't match
-            if (screenDims.x != webcamTexture.width)
-            {
-                InitializeScreen();
-                InitializeCamera(screenDims);
-            }
-        }
-        // If webcam is not being used and it's currently playing, stop it
-        else if (webcamTexture && webcamTexture.isPlaying)
-        {
-            webcamTexture.Stop();
-
-            InitializeScreen();
-            InitializeCamera(screenDims);
-        }
-        // Set imageTexture to screen material main texture if not using webcam
-        else
-        {
-            imageTexture = screen.gameObject.GetComponent<MeshRenderer>().material.mainTexture;
-        }
+        imageTexture = screen.gameObject.GetComponent<MeshRenderer>().material.mainTexture;
 
         // Calculate the input dimensions
+        screenDims.x = (int)screen.transform.localScale.x;
+        screenDims.y = (int)screen.transform.localScale.y;
         Vector2Int inputDims = CalculateInputDims(screenDims, targetDim);
         if (printDebugMessages) Debug.Log($"Input Dims: {inputDims.x} x {inputDims.y}");
 
@@ -462,7 +307,7 @@ public class BarracudaImageClassifier : MonoBehaviour
         if (printDebugMessages) Debug.Log($"Input Dims: {inputTexture.width}x{inputTexture.height}");
 
         // Copy the source texture into the input texture
-        Graphics.Blit((useWebcam ? webcamTexture : imageTexture), inputTexture);
+        Graphics.Blit(imageTexture, inputTexture);
 
         // Check if the model is using a Compute Shader backend
         useAsyncGPUReadback = engine.Summary().Contains("Unity.Barracuda.ComputeVarsWithSharedModel") ? useAsyncGPUReadback : false;
@@ -499,33 +344,6 @@ public class BarracudaImageClassifier : MonoBehaviour
 
         // Unload unused assets in web browser
         if (Application.platform == RuntimePlatform.WebGLPlayer) Resources.UnloadUnusedAssets();
-    }
-
-
-    /// <summary>
-    /// This method is called when the value for the webcam toggle changes
-    /// </summary>
-    /// <param name="useWebcam"></param>
-    public void UpdateWebcamToggle(bool useWebcam)
-    {
-        this.useWebcam = useWebcam;
-    }
-
-
-    /// <summary>
-    /// Set the current webcam device to the one selected in the webcam dropdown
-    /// </summary>
-    public void UpdateWebcamDevice()
-    {
-        currentWebcam = webcamDevices[webcamDropdown.value].name;
-        Debug.Log($"Selected Webcam: {currentWebcam}");
-        
-        // If the useWebcam flag is true, initialize the selected webcam
-        if (useWebcam) InitializeWebcam(currentWebcam);
-
-        // Initialize the screen and camera
-        InitializeScreen();
-        InitializeCamera(screenDims);
     }
 
 
